d2a8c7fb30507aa0e4681d3103ff5772a0dcb7b6
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-21 12:10:07 -0300

    Update loginuser.js

--- users/loginuser.js
+++ users/loginuser.js
@@ -1,3 +1,24 @@
+const { Pool } = require("pg");
+const crypto = require("crypto");
+
+const callconfigs = require("../config");
+
+function createUserId(email) {
+    const emailMd5 = crypto
+        .createHash("md5")
+        .update(email.toLowerCase())
+        .digest("hex");
+
+    const sessionId = crypto.randomUUID();
+
+    return `${sessionId}_${emailMd5}`;
+}
+
+const pool = new Pool({
+    connectionString: callconfigs("postgre"),
+    ssl: { rejectUnauthorized: false }
+});
+
 module.exports = function loginuser(ws, data) {
   ws.send(JSON.stringify({ 
                 message: "userlogued"
