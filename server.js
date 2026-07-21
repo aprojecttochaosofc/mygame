4913e95951d1ea6a6b48206455afc57d87c92709
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-21 17:21:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
+const path = require("path");
 
 const callconfigs = require("./config");
 const homepage = require("./home");
@@ -9,42 +10,17 @@ const loginuser = require("./users/loginuser");
 
 const app = express();
 
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
 app.get("/", (req, res) => {
     homepage(req, res);
 });
+
 app.get("/cadastro", (req, res) => {
     cadastro(req, res);
 });
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", (ws) => {
-
-    ws.on("message", (msg) => {
-
-        let data;
-
-        try {
-            data = JSON.parse(msg.toString());
-        } catch {
-            return;
-        }
-
-        if (data.message === "startserver") {
-            ws.send(JSON.stringify({
-                message: "gamestarted"
-            }));
-        }
-
-        if (data.message === "loginuser") {
-            
-            loginuser(ws, data);
-        }
-
-    });
-
-});
-
-server.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor online");
-});
+// ...
