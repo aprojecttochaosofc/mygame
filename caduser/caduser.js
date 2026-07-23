d15695e0d0ec52d24d168771707ba5c3ee082550
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-23 00:46:43 -0300

    Update caduser.js

--- caduser/caduser.js
+++ caduser/caduser.js
@@ -1,7 +1,63 @@
-module.exports = function cadastro(req, res) {
+const { Pool } = require("pg");
+const crypto = require("crypto");
 
-    res.send(`
-    cadastro
-    `);
+const callconfigs = require("../config");
 
-};
+function createUserId(email) {
+    const emailMd5 = crypto
+        .createHash("md5")
+        .update(email.toLowerCase())
+        .digest("hex");
+
+    const sessionId = crypto.randomUUID();
+
+    return `${sessionId}_${emailMd5}`;
+}
+
+const pool = new Pool({
+    connectionString: callconfigs("postgre"),
+    ssl: { rejectUnauthorized: false }
+});
+
+module.exports = function cadusers(ws, data) {
+ 
+
+
+    async function checkLogin(email, password) {
+
+        try {
+
+            const result = await pool.query(
+                "SELECT * FROM users WHERE email = $1 AND password = $2",
+                [email, password]
+            );
+
+            if (result.rows.length > 0) {
+
+                const userId = createUserId(email);
+
+                 ws.send(JSON.stringify({ 
+                    message: "userlogued",
+                    dados:data
+                }));
+
+            } else {    
+                ws.send(JSON.stringify({
+                    message: "loginfailed"
+                }));
+
+            }
+
+        } catch (err) {
+            ws.send(JSON.stringify({
+                message: "servererror"
+            }));
+
+        }
+    }
+
+    checkLogin(data.email, data.password);
+
+    
+  
+}
