4fe70fbd191f50637fa7cfd669ccc03e8bd8806a
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-24 19:08:52 -0300

    Update loginuser.js

--- users/loginuser.js
+++ users/loginuser.js
@@ -2,74 +2,153 @@ const { Pool } = require("pg");
 const crypto = require("crypto");
 
 const callconfigs = require("../config");
+
+
 function convertmd5(texto) {
+
     return crypto
         .createHash("md5")
         .update(String(texto))
         .digest("hex");
+
 }
+
+
+
 function createUserId(email) {
+
     const emailMd5 = crypto
         .createHash("md5")
         .update(email.toLowerCase())
         .digest("hex");
 
+
     const sessionId = crypto.randomUUID();
 
+
     return `${sessionId}_${emailMd5}`;
+
 }
 
+
+
 const pool = new Pool({
+
     connectionString: callconfigs("postgre"),
-    ssl: {
-        rejectUnauthorized: false
+
+    ssl:{
+        rejectUnauthorized:false
     }
+
 });
 
-module.exports = function loginuser(ws, data) {
- 
 
 
-    async function checkLogin() {
+module.exports = function loginuser(ws, data, players, clients) {
 
-        var email=data.email;
-        var password=convertmd5(data.pass)
 
-        try {
 
-            const result = await pool.query(
-                "SELECT * FROM users WHERE email = $1 AND password = $2",
-                [email, password]
+async function checkLogin(){
+
+
+    var email=data.email;
+
+    var password=convertmd5(data.pass);
+
+
+
+    try{
+
+
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1 AND password = $2",
+            [
+                email,
+                password
+            ]
+        );
+
+
+
+        if(result.rows.length > 0){
+
+
+            const userId = createUserId(email);
+
+
+
+            // guarda quem é esse socket
+            clients.set(
+                ws,
+                userId
             );
 
-            if (result.rows.length > 0) {
 
-                const userId = createUserId(email);
 
-                 ws.send(JSON.stringify({ 
-                    message: "userlogued",
-                     email:email,
-                    userid:userId 
-                }));
-
-            } else {    
-                ws.send(JSON.stringify({
-                    message: "loginfailed"
-                }));
-
-            }
-
-        } catch (err) {
-            console.log("ERRO LOGIN:", err);
             ws.send(JSON.stringify({
-                message: "servererror"
+
+                message:"userlogued",
+
+                email:email,
+
+                userid:userId
+
+            }));
+
+
+
+            // manda os jogadores que já estavam online
+
+            ws.send(JSON.stringify({
+
+                message:"playerssnapshot",
+
+                players:players
+
+            }));
+
+
+
+        }
+        else{
+
+
+            ws.send(JSON.stringify({
+
+                message:"loginfailed"
+
             }));
 
         }
+
+
+
+    }
+    catch(err){
+
+
+        console.log(
+            "ERRO LOGIN:",
+            err
+        );
+
+
+        ws.send(JSON.stringify({
+
+            message:"servererror"
+
+        }));
+
+
     }
 
-    checkLogin();
 
-    
-  
+}
+
+
+
+checkLogin();
+
+
+
 }
