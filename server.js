86f30f507f5e45b7060b6324bb8cc3286a39afa9
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-24 18:38:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,68 +2,208 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const path = require("path");
+
 const callconfigs = require("./config");
 const homepage = require("./home");
+
 const cadusers = require("./caduser/caduser");
 const loginuser = require("./users/loginuser");
 const playerupdate = require("./users/playerupdate");
+const broadcast = require("./users/broadcast");
+
 
 const app = express();
 
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.use(
+    "/assets",
+    express.static(path.join(__dirname, "assets"))
+);
+
+
+
 const players = {};
-app.get("/", (req, res) => {
+const clients = new Map();
+
+
+
+app.get("/", (req,res)=>{
 
     res.sendFile(
-        path.join(__dirname, "index.html")
+        path.join(__dirname,"index.html")
     );
 
 });
 
-app.get("/cadastro", (req, res) => {
-    cadastro(req, res);
+
+
+app.get("/cadastro",(req,res)=>{
+
+    cadastro(req,res);
+
 });
+
+
 
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
 
-wss.on("connection", (ws) => {
 
-    ws.on("message", (msg) => {
+const wss = new WebSocket.Server({
+    server
+});
+
+
+
+
+wss.on("connection",(ws)=>{
+
+
+    ws.on("message",(msg)=>{
+
 
         let data;
 
-        try {
+
+        try{
+
             data = JSON.parse(msg.toString());
-        } catch {
+
+        }
+        catch{
+
             return;
+
         }
 
 
-        if (data.message === "startserver") {
+
+
+        if(data.message === "startserver"){
+
+
             ws.send(JSON.stringify({
-                message: "gamestarted",
+
+                message:"gamestarted",
+
                 datas:data
+
             }));
-        }
-        if (data.message === "caduser") {           
-            cadusers(ws,data);
+
+
         }
 
-        if (data.message === "loginuser") {
-            loginuser(ws, data); 
+
+
+
+
+        if(data.message === "caduser"){
+
+
+            cadusers(
+                ws,
+                data
+            );
+
+
         }
 
-         if (data.message === "playerupdate") {
-            playerupdate(ws, data, players, wss); 
+
+
+
+
+        if(data.message === "loginuser"){
+
+
+            loginuser(
+                ws,
+                data,
+                clients
+            );
+
+
         }
+
+
+
+
+
+
+        if(data.message === "playerupdate"){
+
+
+            playerupdate(
+                ws,
+                data,
+                players,
+                wss
+            );
+
+
+        }
+
+
 
     });
 
+
+
+
+
+    ws.on("close",()=>{
+
+
+        let userid = clients.get(ws);
+
+
+
+        if(userid){
+
+
+            delete players[userid];
+
+
+            clients.delete(ws);
+
+
+        }
+
+
+
+    });
+
+
+
 });
 
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor online");
-});
+
+
+
+
+// envia os jogadores online para todos a cada 100ms
+setInterval(()=>{
+
+
+    broadcast(
+        wss,
+        players,
+        clients
+    );
+
+
+},100);
+
+
+
+
+
+
+server.listen(
+    process.env.PORT || 3000,
+    ()=>{
+
+        console.log("Servidor online");
+
+    }
+);
