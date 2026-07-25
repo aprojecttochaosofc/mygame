0188dfc22d05ec16ad0ed00bef84ba5115735543
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-25 20:34:32 -0300

    Update broadcast.js

--- users/broadcast.js
+++ users/broadcast.js
@@ -4,20 +4,12 @@ module.exports = function broadcast(wss, players, clients) {
 
         if(client.readyState === 1){
 
-            let userid = clients.get(client);
-            let others = {};
-
-            for(let id in players){
-
-                if(id !== userid){
-                    others[id] = players[id];
-                }
-
-            }
-
             client.send(JSON.stringify({
+
                 message:"playerssnapshot",
-                players:others
+
+                players:players
+
             }));
 
         }
