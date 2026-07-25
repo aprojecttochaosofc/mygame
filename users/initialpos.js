16d4b1cf2c515fcd681c8aef720f2903306f4be3
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-25 18:21:25 -0300

    Create initialpos.js

---
+++ users/initialpos.js
@@ -0,0 +1,70 @@
+const { Pool } = require("pg");
+const callconfigs = require("../config");
+
+const pool = new Pool({
+    connectionString: callconfigs("postgre"),
+    ssl:{
+        rejectUnauthorized:false
+    }
+});
+
+
+module.exports = function initialpos(ws, data) {
+
+    async function getPosition(){
+
+        try{
+
+            const result = await pool.query(
+                "SELECT posx, posy, stage FROM users WHERE email = $1",
+                [
+                    data.email
+                ]
+            );
+
+
+            if(result.rows.length > 0){
+
+                ws.send(JSON.stringify({
+
+                    message:"initialpos",
+
+                    posx:result.rows[0].posx,
+
+                    posy:result.rows[0].posy,
+
+                    stage:result.rows[0].stage
+
+                }));
+
+            }
+            else{
+
+                ws.send(JSON.stringify({
+
+                    message:"initialposfail"
+
+                }));
+
+            }
+
+
+        }
+        catch(err){
+
+            console.log("ERRO INITIAL POS:", err);
+
+            ws.send(JSON.stringify({
+
+                message:"servererror"
+
+            }));
+
+        }
+
+    }
+
+
+    getPosition();
+
+};
