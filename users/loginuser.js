abcd9165df5e796e7e00f0735001b19478b4e406
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-25 21:03:44 -0300

    Update loginuser.js

--- users/loginuser.js
+++ users/loginuser.js
@@ -1,4 +1,51 @@
+const { Pool } = require("pg");
+const crypto = require("crypto");
+
+const callconfigs = require("../config");
+
+
+function convertmd5(texto) {
+
+    return crypto
+        .createHash("md5")
+        .update(String(texto))
+        .digest("hex");
+
+}
+
+
+
+function createUserId(email) {
+
+    const emailMd5 = crypto
+        .createHash("md5")
+        .update(email.toLowerCase())
+        .digest("hex");
+
+
+    const sessionId = crypto.randomUUID();
+
+
+    return `${sessionId}_${emailMd5}`;
+
+}
+
+
+
+const pool = new Pool({
+
+    connectionString: callconfigs("postgre"),
+
+    ssl:{
+        rejectUnauthorized:false
+    }
+
+});
+
+
+
 module.exports = function loginuser(ws, data, players, clients, lastPing) {
+
 
 
 async function checkLogin(){
@@ -6,36 +53,47 @@ async function checkLogin(){
 
     var email = data.email;
 
-    var password = convertmd5(data.pass);
-
 
 
     try{
 
+
         let result;
 
 
+
+        // reconexão usando somente email
         if(data.reconnect === true){
 
 
             result = await pool.query(
+
                 "SELECT * FROM users WHERE email = $1",
+
                 [
                     email
                 ]
+
             );
 
 
         }
+        // login normal usando email e senha
         else{
 
 
+            var password = convertmd5(data.pass);
+
+
             result = await pool.query(
+
                 "SELECT * FROM users WHERE email = $1 AND password = $2",
+
                 [
                     email,
                     password
                 ]
+
             );
 
 
@@ -46,13 +104,17 @@ async function checkLogin(){
         if(result.rows.length > 0){
 
 
+
             const userId = createUserId(email);
 
 
 
             clients.set(
+
                 ws,
+
                 userId
+
             );
 
 
@@ -61,41 +123,64 @@ async function checkLogin(){
 
 
 
+
             const user = result.rows[0];
 
 
 
+
+            // coloca somente esse jogador no servidor
+
             players[userId] = {
+
 
                 id:userId,
 
+
                 x:user.posx,
+
 
                 y:user.posy,
 
+
                 stage:user.stage,
 
+
                 animation:"player_idle_down"
+
 
             };
 
 
 
+
+
+            // responde somente para esse cliente
+
             ws.send(JSON.stringify({
+
 
                 message:"userlogued",
 
+
                 email:email,
+
 
                 userid:userId,
 
+
                 posx:user.posx,
+
 
                 posy:user.posy,
 
+
                 stage:user.stage
 
+
+
             }));
+
 
 
 
@@ -103,14 +188,19 @@ async function checkLogin(){
         else{
 
 
+
             ws.send(JSON.stringify({
 
+
                 message:"loginfailed"
+
 
             }));
 
 
+
         }
+
 
 
 
@@ -118,20 +208,29 @@ async function checkLogin(){
     catch(err){
 
 
+
         console.log(
+
             "ERRO LOGIN:",
+
             err
+
         );
+
 
 
         ws.send(JSON.stringify({
 
+
             message:"servererror"
+
 
         }));
 
 
+
     }
+
 
 
 }
@@ -139,6 +238,7 @@ async function checkLogin(){
 
 
 checkLogin();
+
 
 
 }
