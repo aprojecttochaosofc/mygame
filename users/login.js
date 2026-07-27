76681cfab57d561d1fcb4d0d03b905d955a172b1
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-27 17:47:04 -0300

    Update login.js

--- users/login.js
+++ users/login.js
@@ -1,1 +1,25 @@
+module.exports = function login(wss,req, res) {
+wss.on("connection", (ws) => {
 
+    ws.on("message", (msg) => {
+
+        let data;
+
+        try {
+            data = JSON.parse(msg.toString());
+        } catch {
+            return;
+        }
+
+
+       
+
+
+    });
+
+
+    oncloser(ws,data);
+
+
+});
+}
