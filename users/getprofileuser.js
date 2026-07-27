4fc30cab9641c43b45d5e5f551ff64abc40af239
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-27 19:32:46 -0300

    Update getprofileuser.js

--- users/getprofileuser.js
+++ users/getprofileuser.js
@@ -1,42 +1,9 @@
 module.exports = function getprofileuser(ws,data) {
 
 
-    let onlinePlayers = {};
-
-
-    for(let id in players){
-
-
-        if(players[id].online === true){
-
-            onlinePlayers[id] = players[id];
-
-        }
-
-
-    }
-
-
-
-    wss.clients.forEach(client => {
-
-
-        if(client.readyState === 1){
-
-
-            client.send(JSON.stringify({
-
-                message:"playerssnapshot",
-
-                players:onlinePlayers
-
-            }));
-
-
-        }
-
-
-    });
+   ws.send(JSON.stringify({
+       message:"receiveprofile"
+   }));
 
 
 };
