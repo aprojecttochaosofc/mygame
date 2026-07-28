8b2d61fb3f645c1a83d4a825a706f3e4edc1b759
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-27 21:48:15 -0300

    Update getprofileuser.js

--- users/getprofileuser.js
+++ users/getprofileuser.js
@@ -41,35 +41,10 @@ module.exports = async function getprofileuser(ws,data){
 
         const profile=result.rows[0];
  
-        ws.send(JSON.stringify({
-            message:"receiveprofile",
-            nome:profile.nome,
-            apelido:profile.apelido,
-            datanasc:profile.data_nascimento,
-            pais:profile.pais,
-            telefone:profile.telefone,
-            whatsapp:profile.whatsapp,
-            email:profile.email,
-            emailrecuperacao:profile.email_recuperacao,
-            dinheiro:profile.dinheiro,
-            score:profile.score,
-            life:profile.life,
-            lifebar:profile.lifebar,
-            tipoconta:profile.tipo_conta,
-            stage:profile.stage,
-            activity:profile.activity,
-            sessionid:profile.session_id,
-            loginat:profile.login_at,
-            logoutat:profile.logout_at,
-            lastactivity:profile.last_activity,
-            criadoem:profile.criado_em,
-            atualizadoem:profile.atualizado_em,
-            lifeuser:profile.lifeuser,
-            power:profile.power,
-            health:profile.health,
-            estrategy:profile.estrategy,
-            speeduser:profile.speeduser
-        }));
+       ws.send(JSON.stringify({
+        message:"receiveprofile",
+        profile:profile
+    }));
 
     }catch(err){
 
