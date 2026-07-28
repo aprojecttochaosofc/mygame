8359dd33b22e13c5f82392151f94eda04b4864ff
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-27 21:49:18 -0300

    Update getprofileuser.js

--- users/getprofileuser.js
+++ users/getprofileuser.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-const callconfigs = require("../config");
+const callconfigs = require("../config"); 
 
 const pool = new Pool({
     connectionString: callconfigs("postgre"),
